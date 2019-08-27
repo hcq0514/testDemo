ca161e72bbb22eefb1feebf5be200902bf09aff6
hcq <hcq0514@163.com>
2019-08-27 19:54:27 +0800

    SpinLockDemo

--- juc/src/main/java/com/hcq/test/juc/SpinLockDemo.java
+++ juc/src/main/java/com/hcq/test/juc/SpinLockDemo.java
@@ -2,6 +2,7 @@ package com.hcq.test.juc;
 
 import com.hcq.test.juc.util.ThreadUtils;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -13,18 +14,32 @@ import java.util.concurrent.atomic.AtomicReference;
 public class SpinLockDemo {
     public static void main(String[] args) throws Exception {
         SpinLockDemo spinLockDemo = new SpinLockDemo();
-        AtomicReference atomicReference = new AtomicReference(0);
-        ThreadUtils.getThreadPool(1).submit(() -> spinLockDemo.lock1(atomicReference));
-        Thread.sleep(1000);
-        atomicReference.compareAndSet(0, 1);
-        System.out.println("修改atomicReference值为1");
+        ExecutorService threadPool = ThreadUtils.getThreadPool(2);
+        AtomicReference atomicReference = new AtomicReference();
+        threadPool.submit(() -> {
+            spinLockDemo.lock(atomicReference);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.unlock(atomicReference);
+        });
+        threadPool.submit(() -> {
+            spinLockDemo.lock(atomicReference);
+            spinLockDemo.unlock(atomicReference);
+        });
     }
 
-    public void lock1(AtomicReference atomicReference) {
-        System.out.println("enter Lock1");
+    public void lock(AtomicReference atomicReference) {
         //循环等待
-        while (!atomicReference.compareAndSet(1, 2)) {
+        while (!atomicReference.compareAndSet(null, Thread.currentThread())) {
         }
-        System.out.println("修改atomicReference值为" + atomicReference.get());
+        System.out.println("线程" + Thread.currentThread().getName() + "获取锁");
+    }
+
+    public void unlock(AtomicReference atomicReference) {
+        atomicReference.compareAndSet(Thread.currentThread(), null);
+        System.out.println("线程" + Thread.currentThread().getName() + "释放锁");
     }
 }
